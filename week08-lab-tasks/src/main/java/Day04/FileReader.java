0cee1ba4c35e2089c2cdb8742f140a3795198c44
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-04 15:37:19 +0100

    Kiadott feladat:
    Az alábbi fájl beolvasása után készíts egy metódust, amely visszaadja,  hogy melyik nap volt a legkisebb különbség a napi minimum és maximum  hőmérsékletek között! (A feladat megoldásához további segítség a  videóban.)
    
    https://github.com/Training360/java-strukturavalto2-alap/blob/main/consultations/src/main/resources/datamunging/weather.dat
    
    Házi feladat: Az alábbi fájl beolvasása után készíts egy metódust A LEHETŐ  LEGGYORSABBAN, amely visszaadja, hogy melyik focicsapatnál van a  legkisebb különbség a rúgott és a kapott gólok száma között:
    
    https://github.com/Training360/java-strukturavalto2-alap/blob/main/consultations/src/main/resources/datamunging/football.dat
    ----------------------------------------------
    Kiadott feladat:
    
    Úgy refactoráld (módosítsd a funkcionalitás változtatása nélkül) a tegnap, házi feladatként megírt kódodat, hogy a lehető legkevesebb kódismétlés legyen benne. Ne feledd, hogy csak úgy tudod biztosítani, hogy az eredmény nem változik, hogy előtte teszteket írsz! (További segítség a videóban.)

---
+++ week08-lab-tasks/src/main/java/Day04/FileReader.java
@@ -0,0 +1,55 @@
+package Day04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReader {
+    public int findSmallestTemperatureSpread(String pathString) {
+        int min = Integer.MAX_VALUE;
+        int dayOfMinTemp = -1;
+        Path path = Paths.get("src/main/resources/" + pathString);
+        List<String> contents;
+        try {
+            contents = Files.readAllLines(path);
+        } catch (IOException err) {
+            throw new IllegalStateException("Error reading file.", err);
+        }
+        for (int i = 2; i < 32; i++) {
+            try {
+                int day = Integer.parseInt(contents.get(i).substring(2, 4).trim());
+                int dailyMax = Integer.parseInt(contents.get(i).substring(6, 8).trim());
+                int dailyMin = Integer.parseInt(contents.get(i).substring(12, 14).trim());
+                if (dailyMax - dailyMin < min) {
+                    min = dailyMax - dailyMin;
+                    dayOfMinTemp = day;
+                }
+            } catch (NumberFormatException err) {
+                throw new IllegalArgumentException("Invalid data.", err);
+            }
+        }
+        return dayOfMinTemp;
+    }
+
+    public String findSmallestDifference() throws IOException {
+        Path path = Paths.get("src/main/resources/football.dat");
+        List<String> contents = Files.readAllLines(path);
+        int min = Integer.MAX_VALUE;
+        String result = "";
+        for (String line : contents) {
+            try {
+                int score = Integer.parseInt(line.substring(43, 45).trim());
+                int got = Integer.parseInt(line.substring(50, 52).trim());
+                if (Math.abs(score - got) < min) {
+                    min = Math.abs(score - got);
+                    result = line.substring(6, 23).trim();
+                }
+            } catch (NumberFormatException err) {
+                continue;
+            }
+        }
+        return result;
+    }
+}
---
+++ week08-lab-tasks/src/test/java/Day04/FileReaderTest.java
@@ -0,0 +1,28 @@
+package Day04;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileReaderTest {
+    @Test
+    void testFileReader(){
+        String filename = "weather.dat";
+        int result = new FileReader().findSmallestTemperatureSpread(filename);
+        assertEquals(14, result);
+    }
+
+    @Test
+    void testFileReaderFileNotExists(){
+        IllegalStateException err = assertThrows(IllegalStateException.class,
+                () -> new FileReader().findSmallestTemperatureSpread("filename"));
+        assertEquals("Error reading file.", err.getMessage());
+    }
+
+    @Test
+    void testFindSmallestDifference() throws IOException {
+        assertEquals("Aston_Villa", new FileReader().findSmallestDifference());
+    }
+}

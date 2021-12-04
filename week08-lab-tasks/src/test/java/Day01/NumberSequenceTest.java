c5fa7df7758098c47e247d3238699bfbb5d2ec0e
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-12-04 15:04:11 +0100

    Készíts egy day01.NumberSequence osztályt, aminek van egy  egész számokból álló listája. Vagy egy kész listát kap konstruktoron  keresztül és ez lesz az attribútum értéke, vagy három számot kap,  melyből az első hogy hány véletlen számot generáljon a másik kettő pedig a minimum és maximum érték ami között generáljon. Legyen egy metódus closeToAverage(int value) ,ami kigyűjti és visszaóadja azokat a számokat egy listában, amik az  átlagtól legfeljebb a paraméterül átadott értékben térnek el!-

---
+++ week08-lab-tasks/src/test/java/Day01/NumberSequenceTest.java
@@ -0,0 +1,50 @@
+package Day01;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NumberSequenceTest {
+    @Test
+    void test() {
+        List<Integer> numbers = new ArrayList<>(10);
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+        NumberSequence ns1 = new NumberSequence(numbers);
+        assertEquals(numbers, ns1.closeToAverage(5));
+
+        numbers.remove(9);
+        numbers.remove(0);
+        assertEquals(numbers, ns1.closeToAverage(4));
+
+        assertEquals(Arrays.asList(4, 5, 6, 7), ns1.closeToAverage(2));
+        assertEquals(10, ns1.getNumbers().size());
+    }
+
+    @Test
+    void testRandom() {
+        List<Integer> numbers = new NumberSequence(100, 15, 25).getNumbers();
+        assertEquals(100, numbers.size());
+
+        int minValue = numbers.get(0);
+        int maxValue = numbers.get(0);
+        List<Integer> distincts = new ArrayList<>();
+        for (int number : numbers) {
+            if (!distincts.contains(number)) {
+                minValue = Math.min(number, minValue);
+                maxValue = Math.max(number, maxValue);
+                distincts.add(number);
+            }
+        }
+        assertTrue(15 <= minValue);
+        assertTrue(25 >= maxValue);
+        assertTrue(8 <= distincts.size());
+        assertTrue(distincts.size() <= 11);
+    }
+}
